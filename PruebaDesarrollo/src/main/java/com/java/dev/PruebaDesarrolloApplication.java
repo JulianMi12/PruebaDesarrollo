ccd51115dfd994a46d4611f0cc018e475ac3c128
Julian <julianmiranda1@hotmail.com>
2021-06-22 17:13:37 -0500

    Primer commit del proyecto.

---
+++ PruebaDesarrollo/src/main/java/com/java/dev/PruebaDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.java.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDesarrolloApplication.class, args);
+	}
+
+}
